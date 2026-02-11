2a9aae44662fe2768822e174e6b747abafeb9bf4
Ad byeon <minjunbyeopn@gmail.com>
2026-02-11 13:12:14 +0900

    Firebase 설정 파일 및 관리자 페이지 추가

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+/**
+ * Firebase Configuration - Gangseo Senior Club
+ * 공개된 클라이언트 설정 (API Key는 클라이언트용이며 보안 위험 없음)
+ */
+const firebaseConfig = {
+    apiKey: "AIzaSyDQu8Tdgcs37KMkR9Qvk6NsrZisfBoR_-w",
+    authDomain: "gangseo-senior.firebaseapp.com",
+    projectId: "gangseo-senior",
+    storageBucket: "gangseo-senior.firebasestorage.app",
+    messagingSenderId: "1064841165347",
+    appId: "1:1064841165347:web:11965a055f5e3b3d95cf96"
+};
